ba0296a440af176d95b75c8ec4818e3e921e5afe
Dan Billson <dbillson@outlook.com>
2023-12-04 11:39:11 +0000

    📇 day 4:2

--- src/04/index.ts
+++ src/04/index.ts
@@ -5,7 +5,7 @@ const data = await file.text();
 const lines = data.split("\n") as Line[];
 
 console.log("Part 1:", part1());
-// console.log("Part 2:", part2());
+console.log("Part 2:", part2());
 
 type Line = `Card ${number}: ${string} | ${string}`;
 
@@ -32,4 +32,28 @@ function part1() {
 
     return acc + score(matches.length);
   }, 0);
+}
+
+function part2() {
+  const matches = lines.reduce((acc, cur) => {
+    const [, numbers] = cur.split(":");
+    const [winning, yours] = numbers
+      .split("|")
+      .map((x) => x.trim().split(" ").filter(Boolean));
+
+    const matches = yours.filter((x) => winning.includes(x));
+
+    return [...acc, matches.length];
+  }, [] as number[]);
+
+  const copies = Array.from({ length: matches.length }, () => 1);
+  for (let i = 0; i < copies.length; i++) {
+    for (let j = 1; j <= matches[i]; j++) {
+      for (let k = 0; k < copies[i]; k++) {
+        copies[i + j] += 1;
+      }
+    }
+  }
+
+  return copies.reduce((acc, cur) => acc + cur, 0);
 }
