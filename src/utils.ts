6b87367d7e65d855b885a73173c9d18bf0047d12
Dan Billson <dbillson@outlook.com>
2023-12-13 07:21:19 +0000

    🩺 add util for pretty printing matrices

---
+++ src/utils.ts
@@ -0,0 +1,30 @@
+function columns(matrix: string[][], i: number) {
+  return matrix.map((row) => row[i]);
+}
+
+export function logMatrix(matrix: string[][]) {
+  let shape = [matrix.length, matrix[0].length];
+  let colMaxes: number[] = [];
+
+  for (let i = 0; i < shape[1]; i++) {
+    colMaxes.push(
+      Math.max.apply(
+        null,
+        columns(matrix, i).map((n) => n.toString().length)
+      )
+    );
+  }
+
+  matrix.forEach((row) => {
+    console.log.apply(
+      null,
+      row.map((val, j) => {
+        return (
+          new Array(colMaxes[j] - val.toString().length + 1).join(" ") +
+          val.toString() +
+          "  "
+        );
+      })
+    );
+  });
+}
