e108aa3852d08ffbc33fa32b3c15e14644d3710c
Dan Billson <dbillson@outlook.com>
2023-12-15 11:43:04 +0000

    🔦 day 15:2

--- src/15/index.ts
+++ src/15/index.ts
@@ -16,5 +16,59 @@ function part1() {
   return sequence.reduce((acc, cur) => acc + hash(cur), 0);
 }
 
+function createInstructions(list: string[]) {
+  return list.map((chars) => {
+    // [lens label, operation character, lens focal length]
+    const [label, op, fl] = chars.split(/([-=])/);
+    return {
+      key: label,
+      box: hash(label),
+      label: `${label} ${fl}`,
+      op,
+    };
+  });
+}
+
+type Instruction = ReturnType<typeof createInstructions>[number];
+
+function moveBoxes(instructions: Instruction[]) {
+  return instructions.reduce((acc, cur) => {
+    if (cur.op === "=") {
+      const current = acc[cur.box] || [];
+      const newBox = current.some((l) => l.startsWith(cur.key))
+        ? current.map((l) => (l.startsWith(cur.key) ? cur.label : l))
+        : [...current, cur.label];
+      return { ...acc, [cur.box]: newBox };
+    } else {
+      const current = acc[cur.box] || [];
+      const newBox = current.filter((l) => !l.startsWith(cur.key));
+      return { ...acc, [cur.box]: newBox };
+    }
+  }, {} as Record<string, string[]>);
+}
+
+function lensPower(box: number, slot: number, focalLength: number) {
+  return box * slot * focalLength;
+}
+
+function focusingPower(boxes: Record<string, string[]>) {
+  return Object.entries(boxes).reduce((acc, cur) => {
+    const [box, labels] = cur;
+
+    const power = labels.reduce((acc, cur, i) => {
+      const [, fl] = cur.split(" ");
+      return acc + lensPower(Number(box) + 1, i + 1, Number(fl));
+    }, 0);
+
+    return acc + power;
+  }, 0);
+}
+
+function part2() {
+  const instructions = createInstructions(sequence);
+  const boxes = moveBoxes(instructions);
+  return focusingPower(boxes);
+}
+
 console.log(part1());
-// console.log(part2());
+console.log(part2());
