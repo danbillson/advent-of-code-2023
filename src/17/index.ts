4babc9a572876362b0efd60c1f296ea0b423e75a
Dan Billson <dbillson@outlook.com>
2023-12-17 19:22:17 +0000

    😢 day 17:2

--- src/17/index.ts
+++ src/17/index.ts
@@ -40,7 +40,6 @@ function shortestPath(grid: number[][]) {
 
   while (!pq.isEmpty()) {
     const { hl, r, c, dr, dc, n } = pq.dequeue() as Node;
-    console.log(hl, r, c, dr, dc, n);
 
     if (r === city.length - 1 && c === city[0].length - 1) return hl;
 
@@ -89,5 +88,65 @@ function part1() {
   return shortestPath(city);
 }
 
-console.log(part1());
-// console.log(part2());
+function shortestUltraPath(grid: number[][]) {
+  const city = [...grid];
+  const pq = new PriorityQueue();
+  const seen = new Set<string>();
+
+  pq.enqueue({ hl: 0, r: 0, c: 0, dr: 0, dc: 0, n: 0 }, 0);
+
+  while (!pq.isEmpty()) {
+    const { hl, r, c, dr, dc, n } = pq.dequeue() as Node;
+    console.log(hl, r, c, dr, dc, n);
+
+    if (r === city.length - 1 && c === city[0].length - 1) return hl;
+
+    const key = `${r}:${c}:${dr}:${dc}:${n}`;
+
+    if (seen.has(key)) continue;
+    seen.add(key);
+
+    if (n < 10 && `${dr}:${dc}` !== `0:0`) {
+      const nr = r + dr;
+      const nc = c + dc;
+      if (nr >= 0 && nc >= 0 && nr < city.length && nc < city[0].length) {
+        pq.enqueue(
+          { hl: hl + city[nr][nc], r: nr, c: nc, dr, dc, n: n + 1 },
+          hl + city[nr][nc]
+        );
+      }
+    }
+
+    if (n >= 4 || `${dr}:${dc}` === `0:0`) {
+      for (let [ndr, ndc] of [
+        [0, 1],
+        [1, 0],
+        [0, -1],
+        [-1, 0],
+      ]) {
+        if (
+          `${ndr}:${ndc}` !== `${dr}:${dc}` &&
+          `${ndr}:${ndc}` !== `${-dr}:${-dc}`
+        ) {
+          const nr = r + ndr;
+          const nc = c + ndc;
+          if (nr >= 0 && nc >= 0 && nr < city.length && nc < city[0].length) {
+            pq.enqueue(
+              { hl: hl + city[nr][nc], r: nr, c: nc, dr: ndr, dc: ndc, n: 1 },
+              hl + city[nr][nc]
+            );
+          }
+        }
+      }
+    }
+  }
+
+  return -1;
+}
+
+function part2() {
+  return shortestUltraPath(city);
+}
+
+// console.log(part1());
+console.log(part2());
