3d93eb93fa667af624a0757e9bc789761d533a61
Dan Billson <dbillson@outlook.com>
2023-12-17 08:38:01 +0000

    ♻️ refactor: day 14 without transpose

--- src/14/index.ts
+++ src/14/index.ts
@@ -1,44 +1,44 @@
-import { logMatrix } from "../utils";
-
 const path = "src/14/data.txt";
 const file = Bun.file(path);
 
 const data = await file.text();
 const grid = data.split("\n").map((line) => line.split(""));
 
-function transpose(matrix: string[][]) {
-  return matrix[0].map((_, colIndex) => matrix.map((row) => row[colIndex]));
-}
-
-function tilt(line: string[]) {
-  const row = [...line];
-  for (let i = 0; i < row.length; i++) {
-    if (row[i] === "O") {
-      for (let j = i; j >= 0; j--) {
-        if (row[j - 1] === "#") break;
-        if (row[j - 1] === ".") {
-          row[j - 1] = "O";
-          row[j] = ".";
+function tiltNorth(grid: string[][]) {
+  for (let col = 0; col < grid[0].length; col++) {
+    for (let row = 0; row < grid.length; row++) {
+      if (grid[row][col] === "O") {
+        for (let i = row; i >= 0; i--) {
+          if (grid[i - 1]?.[col] === "#") break;
+          if (grid[i - 1]?.[col] === ".") {
+            grid[i - 1][col] = "O";
+            grid[i][col] = ".";
+          }
         }
       }
     }
   }
-  return row;
+
+  return grid;
+}
+
+function getScore(grid: string[][]) {
+  let score = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "O") {
+        score += grid.length - row;
+      }
+    }
+  }
+
+  return score;
 }
 
 function part1() {
-  const rotated = transpose(grid);
-
-  return rotated.reduce((acc, cur) => {
-    const tilted = tilt(cur);
-    const score = tilted.reduce((acc, cur, i) => {
-      if (cur === "O") {
-        return acc + (tilted.length - i);
-      }
-      return acc;
-    }, 0);
-    return acc + score;
-  }, 0);
+  const titled = tiltNorth(grid);
+  return getScore(titled);
 }
 
 console.log(part1());
